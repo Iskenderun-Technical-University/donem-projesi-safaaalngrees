599fa7dea93bf9494820bfbd1ed11da0b558cd72
SAFAA ALABD <safaaalngrees.mdbf20@iste.edu.tr>
2023-05-01 01:20:35 +0300

    student manage kodlamasi

--- Student Management System/students.cs
+++ Student Management System/students.cs
@@ -33,9 +33,42 @@ namespace student_management_system
             DepCb.ValueMember = Functions.GetData(Query).Columns["DepId"].ToString();
             DepCb.DataSource = Functions.GetData(Query);
         }
+        private void clear()
+        {
+            StNameTb.Text = "";
+            StPhoneTb.Text = "";
+            StParentTb.Text = "";
+            StAddTb.Text = "";
+            GenCb.SelectedIndex = -1;
+        }
         private void AddBtn_Click(object sender, EventArgs e)
         {
-
+            if (StNameTb.Text == "" || StPhoneTb.Text == "" ||StParentTb.Text == "" ||StAddTb.Text == "" || DepCb.SelectedIndex == -1 || GenCb.SelectedIndex == -1 )
+            {
+                MessageBox.Show("Missing Data!!");
+            }
+            else
+            {
+                try
+                {
+                    string TName = StNameTb.Text;
+                    string Gender = GenCb.SelectedItem.ToString();
+                    string Phone = StPhoneTb.Text;
+                    string Parent = StParentTb.Text;
+                    string Address = StAddTb.Text;
+                    int Dep = Convert.ToInt32(DepCb.SelectedValue.ToString());
+                    string Query = "insert into studentTb1 valuse('{0}','{1}','{2}','{3}','{4}','{5}')";
+                    Query = string.Format(Query, TName, Gender,Phone,Parent,Address,Dep);
+                 
+                    showStudents();
+                    MessageBox.Show("Student Added !!!");
+                    clear();
+                }
+                catch (Exception Ex)
+                {
+                    MessageBox.Show(Ex.Message);
+                }
+            }
         }
     }
 }
